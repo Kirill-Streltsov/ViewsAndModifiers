0ba8971c044e79ae101947897a165bae5c49974a
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-07-28 17:52:58 +0200

    finished Project

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,12 +7,78 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding()
+        }
+    }
+}
+
+struct CapsuleText: View {
+    var text: String
+    var body: some View {
+        Text(text)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(Capsule())
+            .shadow(radius: 5)
+        
+    }
+}
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
     var body: some View {
         VStack {
-            Text("Hello, world!")
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+
+struct ContentView: View {
+    var body: some View {
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+        }
     }
 }
 
